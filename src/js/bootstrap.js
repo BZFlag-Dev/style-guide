12d7c88b388c087ea6d2e8c6ebe54ecc0edc8626
Vladimir Jimenez <allejo@me.com>
2019-06-23 13:46:13 -0700

    Add bootstrap file to run JS at page load
    Not to be confused with the CSS framework

---
+++ src/js/bootstrap.js
@@ -0,0 +1,7 @@
+(function () {
+    // Say we have JS enabled in the browser
+    const noJS = document.querySelector('.no-js');
+
+    noJS.classList.remove('no-js');
+    noJS.classList.add('js');
+})();
